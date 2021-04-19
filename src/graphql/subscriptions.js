7f03a534037318d7d847f65529d32ca796b1c23c
Ahmed <ahmedi3aaa@hotmail.com>
2021-04-19 07:19:37 +0200

    make user model

---
+++ src/graphql/subscriptions.js
@@ -0,0 +1,39 @@
+/* eslint-disable */
+// this is an auto generated file. This will be overwritten
+
+export const onCreateUser = /* GraphQL */ `
+  subscription OnCreateUser {
+    onCreateUser {
+      id
+      fullName
+      imageUrl
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onUpdateUser = /* GraphQL */ `
+  subscription OnUpdateUser {
+    onUpdateUser {
+      id
+      fullName
+      imageUrl
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
+export const onDeleteUser = /* GraphQL */ `
+  subscription OnDeleteUser {
+    onDeleteUser {
+      id
+      fullName
+      imageUrl
+      status
+      createdAt
+      updatedAt
+    }
+  }
+`;
